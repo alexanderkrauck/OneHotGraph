a060a7338106b49f1ed0de4348c8a4ddada6d773
krauck <alexander.krauck@gmail.com>
2021-08-24 12:41:12 +0200

    Isomorphism One Hot Graph

--- utils/baselines.py
+++ utils/baselines.py
@@ -7,7 +7,7 @@ __author__ = "Alexander Krauck"
 __email__ = "alexander.krauck@gmail.com"
 __date__ = "21-08-2021"
 
-from utils.one_hot_graph import OneHotGraph
+from utils.one_hot_graph import AttentionOneHotConv, IsomporphismOneHotConv, OneHotGraph
 from torch import nn
 from utils.basic_modules import GIN, MLP, GAT, GINGAT
 from torch_geometric import nn as gnn
@@ -90,10 +90,45 @@ class GINGAT_Baseline(nn.Module):
         
         return x
 
-class OneHotGraph_Baseline(nn.Module):
+class AttentionOneHotGraph_Baseline(nn.Module):
     def __init__(self, data_module, n_hidden_channels, n_graph_layers, n_graph_dropout, n_linear_layers, n_linear_dropout, **kwargs):
-        super(OneHotGraph_Baseline, self).__init__()
-        self.ohg = OneHotGraph(data_module.num_node_features, n_hidden_channels, n_graph_layers, dropout=n_graph_dropout, **kwargs)
+        super(AttentionOneHotGraph_Baseline, self).__init__()
+
+        self.ohg = OneHotGraph(
+            data_module.num_node_features, 
+            n_hidden_channels,
+            n_graph_layers, 
+            dropout=n_graph_dropout, 
+            conv_type=AttentionOneHotConv, 
+            **kwargs
+            )
+        
+        self.head = MLP(n_linear_layers, n_hidden_channels, n_hidden_channels, data_module.num_classes, n_linear_dropout, nn.ReLU())
+
+    def forward(self, x, edge_index, batch_sample_indices, n_sample_nodes, adjs, **kwargs):
+        # 1. Obtain node embeddings 
+        x = self.ohg(x, edge_index, batch_sample_indices, n_sample_nodes, adjs)
+
+        # 2. Readout layer
+        x = gnn.global_mean_pool(x, batch_sample_indices)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        x =  self.head(x)
+        
+        return x
+
+class IsomorphismOneHotGraph_Baseline(nn.Module):
+    def __init__(self, data_module, n_hidden_channels, n_graph_layers, n_graph_dropout, n_linear_layers, n_linear_dropout, **kwargs):
+        super(IsomorphismOneHotGraph_Baseline, self).__init__()
+
+        self.ohg = OneHotGraph(
+            data_module.num_node_features, 
+            n_hidden_channels, 
+            n_graph_layers, 
+            dropout=n_graph_dropout, 
+            conv_type=IsomporphismOneHotConv, 
+            **kwargs
+            )
         
         self.head = MLP(n_linear_layers, n_hidden_channels, n_hidden_channels, data_module.num_classes, n_linear_dropout, nn.ReLU())
 
--- utils/one_hot_graph.py
+++ utils/one_hot_graph.py
@@ -24,7 +24,7 @@ import torch_geometric
 
 
 
-class OneHotConv(nn.Module):
+class AttentionOneHotConv(nn.Module):
 
     def __init__(
         self, 
@@ -42,7 +42,7 @@ class OneHotConv(nn.Module):
         **kwargs
         ):
 
-        super(OneHotConv, self).__init__()
+        super(AttentionOneHotConv, self).__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -282,6 +282,192 @@ class OneHotConv(nn.Module):
 
         return aggregated_selected_x, new_onehots
 
+class IsomporphismOneHotConv(nn.Module):
+
+    def __init__(
+        self, 
+        in_channels: Union[int, Tuple[int, int]],
+        out_channels: int, 
+        heads: int = 1, 
+        concat: bool = True,
+        negative_slope: float = 0.2, 
+        dropout: float = 0.0,
+        one_hot_mode: str = "conv",
+        one_hot_channels: int = 8,
+        **kwargs
+        ):
+
+        super(IsomporphismOneHotConv, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.heads = heads
+        self.concat = concat
+        self.negative_slope = negative_slope
+        self.dropout = dropout
+        self.one_hot_mode = one_hot_mode
+        self.one_hot_cannels = one_hot_channels
+
+        self.mlp = nn.Sequential(
+            nn.Linear(in_channels + one_hot_channels, out_channels),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Linear(out_channels, out_channels),
+        )
+
+        if self.one_hot_mode == "conv":
+            self.onehot_pipe = nn.Sequential(#This is very arbitrary
+                nn.Conv1d(1, 8, 3, padding=1),
+                nn.ReLU(),
+                nn.Conv1d(8, 16, 3, padding=1),
+                nn.ReLU(),
+                nn.AdaptiveAvgPool1d(1),
+                nn.Flatten(),
+                nn.Linear(16, one_hot_channels)
+            )
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        pass #TODO
+
+    def index_select_onehots(self, onehots, adjs):
+            
+        selected_onehots = []
+
+        for onehot, adj in zip(onehots, adjs):
+            selected_onehot = onehot.index_select(0, adj[0])
+            selected_onehots.append(selected_onehot)
+
+        return selected_onehots
+
+    def forward(
+        self, 
+        x: Union[torch.Tensor, OptPairTensor], 
+        onehots: List[Tensor], 
+        edge_index: Adj, 
+        batch_sample_indices: Tensor,
+        n_sample_nodes: Tensor,
+        adjs: List[Tensor],
+        **kwargs
+        ):
+        """
+
+        Parameters
+        ----------
+        x: Union[torch.Tensor, OptPairTensor]
+            The main channels of the graph-nodes
+        onehots: List[Tensor]
+            The 'special' one-hot channels of each node. Each list element corresponds to one graph
+        edge_index: Adj
+            The sending and receiving graph connection. I.e. the adjacent matrix of all the graphs in one batch
+        batch_sample_indices: Tensor
+            The index of the sample inside the minibatch the node corresponds to
+        n_sample_nodes: Tensor
+            For each graph in the minibatch the number of nodes
+        adjs: List[Tensor]
+            The same as in edge_index but seperate for each graph in the minibatch
+        """
+        
+        x, onehots = self.propagate(
+            index = edge_index, 
+            onehots = onehots, 
+            sample_indices = batch_sample_indices, 
+            x = x, 
+            n_sample_nodes = n_sample_nodes,
+            adjs = adjs
+        )
+
+        #Onehot Convolution (mine)
+        if self.one_hot_mode == "conv":
+            onethot_res = []
+            for onehot in onehots:
+                onehot, _ = onehot.sort(dim = -1)#sort such that the pattern is invariant
+
+                onethot_res.append(self.onehot_pipe(onehot.unsqueeze(1)))
+            onethot_res = torch.vstack(onethot_res)
+
+            x = torch.hstack((x, onethot_res))
+        
+        x = self.mlp(x)
+
+
+        return x, onehots
+
+    def propagate(self, x, onehots, index, sample_indices, n_sample_nodes, adjs, **kwargs):
+        sending_indices = index[0]
+        receiving_indices = index[1]
+
+        selected_onehots = self.index_select_onehots(onehots, adjs)
+        selected_x = x.index_select(0, sending_indices)
+
+        weighted_selected_x, weighted_selected_onethots = self.message(
+            selected_x = selected_x, 
+            selected_onehots = selected_onehots, 
+            sample_indices = sample_indices, 
+            n_sample_nodes = n_sample_nodes, 
+            receiving_index = receiving_indices
+        )
+
+        aggregated_selected_x, aggregated_selected_onehots = self.aggregate(
+            weighted_selected_x, 
+            weighted_selected_onethots, 
+            receiving_indices,
+            adjs,
+            n_sample_nodes
+        )
+
+        new_x, new_onehots = self.update(
+            aggregated_selected_x,
+            aggregated_selected_onehots,
+            onehots
+        )
+
+        return new_x, new_onehots
+
+    def message(
+        self, 
+        selected_x: Tensor, 
+        selected_onehots: Tensor, 
+        receiving_index: Tensor, 
+        **kwargs
+        ) -> Tuple[Tensor, List[Tensor]]:
+
+
+        
+        return selected_x, selected_onehots
+
+    def aggregate(
+        self,
+        weighted_selected_x, 
+        weighted_selected_onethots, 
+        receiving_indices: Tensor, 
+        adjs,
+        n_sample_nodes: Tensor,
+        **kwargs
+        ) -> Tuple[Tensor, List[Tensor]]:
+
+        aggregated_selected_x = torch_scatter.scatter(weighted_selected_x, receiving_indices, dim=0, dim_size=n_sample_nodes.sum(), reduce="sum")
+        
+        aggregated_selected_onehots = []
+        for ws_onehot, adj, n_nodes in zip(weighted_selected_onethots, adjs, n_sample_nodes):
+            aggregated_selected_onehots.append(torch_scatter.scatter(ws_onehot, adj[1], dim=0, dim_size=n_nodes, reduce="add"))
+
+        return aggregated_selected_x, aggregated_selected_onehots
+
+    def update(
+        self, 
+        aggregated_selected_x: Tensor, 
+        aggregated_selected_onehots: List[Tensor],
+        onehots: List[Tensor],
+        **kwargs
+        ) -> Tuple[Tensor, List[Tensor]]:
+
+        new_onehots = []
+        for idx in range(len(aggregated_selected_onehots)):
+            new_onehots.append(aggregated_selected_onehots[idx] + onehots[idx])
+
+        return aggregated_selected_x, new_onehots
 
 
 
@@ -295,19 +481,24 @@ class OneHotGraph(nn.Module):
         dropout: float = 0.0,
         heads = 1,
         act: Optional[Callable] = nn.ReLU(inplace=True),
+        conv_type = AttentionOneHotConv,
         **kwargs
         ):
         super(OneHotGraph, self).__init__()
 
-        assert hidden_channels % heads == 0
 
-        out_channels = hidden_channels // heads
+        if conv_type == AttentionOneHotConv:
+            assert hidden_channels % heads == 0
+            out_channels = hidden_channels // heads
+        if conv_type == IsomporphismOneHotConv:
+            out_channels = hidden_channels
+
 
         self.convs = nn.ModuleList()
 
-        self.convs.append(OneHotConv(in_channels, out_channels, dropout=dropout, heads = heads, **kwargs))
+        self.convs.append(conv_type(in_channels, out_channels, dropout=dropout, heads = heads, **kwargs))
         for _ in range(1, num_layers):
-            self.convs.append(OneHotConv(hidden_channels, out_channels, heads = heads, **kwargs))
+            self.convs.append(conv_type(hidden_channels, out_channels, heads = heads, **kwargs))
 
 
         self.dropout = dropout
