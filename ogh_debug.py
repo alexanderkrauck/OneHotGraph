1271682f94780c466dbb88d19e89fed20b5e4373
krauck <alexander.krauck@gmail.com>
2021-08-24 16:50:33 +0200

    Experimenting

--- ogh_debug.py
+++ ogh_debug.py
@@ -2,8 +2,10 @@
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
 from utils import training, data
-from utils.one_hot_graph import OneHotConv, OneHotGraph
+from utils.one_hot_graph import AttentionOneHotConv, OneHotGraph
 import torch
+from utils.baselines import IsomorphismOneHotGraph_Baseline
+import utils.training
 
 
 # %%
@@ -12,20 +14,23 @@ x_size = 2
 
 
 # %%
-ohg_conv = OneHotConv(input_channels, 5, 1)
+data_module = data.DataModule("tox21_original", split_mode = "predefined", workers = 2)
+one_hot_channels=8
+ohg_conv = IsomorphismOneHotGraph_Baseline(data_module, 128, 2, 0.2, 2, 0.2, one_hot_channels=one_hot_channels).to("cuda")
 
 
 # %%
-x = torch.randn(x_size, input_channels)
-onehots = [torch.eye(x_size)]
-index = torch.randint(0,2, (x_size, x_size))
-index = index + index.T
-index = index.to_sparse()._indices()
-adjs = [index]
+a = []
+for epoch in range(20):
+    for n, p in ohg_conv.named_parameters():
+        if n == "ohg.convs.0.mlp.0.weight":
+            a.append(p)
+    opt = torch.optim.Adam(ohg_conv.parameters(), lr=1e-3, weight_decay=1e-8)
+    utils.training.train(ohg_conv, opt, data_module.make_train_loader(), data_module.num_classes, epoch, device="cuda", use_tqdm=True)
 
 # %%
-print(f"OneHots:\n{onehots}")
-print(f"x:\n{x}")
+print(a[0][:, : -one_hot_channels].detach().abs().mean())
+print(a[0][:, -one_hot_channels:].detach().abs().mean())
 
 # %%
 x, onehots = ohg_conv.forward(x, onehots, index, torch.ones(len(x)), torch.tensor([x_size]), adjs)
